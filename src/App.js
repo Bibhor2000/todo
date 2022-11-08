a0e940b490102a3cfdec33c4b5fb51837bd0f09d
Bibhor2000 <daxter740@gmail.com>
2022-11-07 20:34:49 -0500

    added more notes/comments to the code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 //Function for shaping the appearance of a task element
-function Task({ task, index, markTask, removeTask }) {
+function Task({ task, indexPosition, markTask, removeTask }) {
   return (
       <div
           className="task"
@@ -11,15 +11,15 @@ function Task({ task, index, markTask, removeTask }) {
       >
           {task.title}
 
-          <button style={{ background: "red" }} onClick={() => removeTask(index)}>Remove</button> 
-          <button onClick={() => markTask(index)}>Complete</button>
+          <button style={{ background: "red" }} onClick={() => removeTask(indexPosition)}>Remove</button> 
+          <button onClick={() => markTask(indexPosition)}>Complete</button>
 
       </div>
   );
 }
 
 //Function for creating the task
-function CreateTask({ addTask }) { //addTask is a prop here
+function Create({ addTask }) { //addTask is a prop here
   const [value, setValue] = useState(""); //two elements with empty strings
 
   const handleSubmit = event => { //if no value, calls the addTask function with the input value
@@ -37,7 +37,7 @@ function CreateTask({ addTask }) { //addTask is a prop here
               value={value}
               placeholder="Add a new task"
               onChange={event => setValue(event.target.value)}
-          />
+          /> {/* event is passed in to access the value of a changed element */}
       </form>
   );
 }
@@ -69,15 +69,15 @@ function App () {
       setTasks(newTasks);
   };
   
-  const markTask = index => {
+  const markTask = indexPosition => {
       const newTasks = [...tasks];
-      newTasks[index].complete = true;
+      newTasks[indexPosition].complete = true;
       setTasks(newTasks);
   };
   
-  const removeTask = index => {
+  const removeTask = indexPosition => {
       const newTasks = [...tasks];
-      newTasks.splice(index, 1);
+      newTasks.splice(indexPosition, 1);
       setTasks(newTasks);
   };
 
@@ -85,19 +85,19 @@ function App () {
       <div className="todo-container">
           <div className="header">Pending tasks ({tasksRemaining})</div>
           <div className="tasks">
-              {tasks.map((task, index) => (
+              {tasks.map((task, indexPosition) => (
                   <Task
                   task={task}
-                  index={index}
-                  markTask={markTask}
+                  indexPosition={indexPosition}
+                  markTask={markTask} 
                   removeTask={removeTask}
-                  key={index}
-                  />
-              ))}
+                  key={indexPosition}
+                  /> 
+              ))} {/*Passed down from line 6 as props to call the respective functions*/}
           </div>
           <div className="create-task" >
-              <CreateTask addTask={addTask} />
-          </div>
+              <Create addTask={addTask} />
+          </div> {/*Passed down from line 22 as props to call the addTask function*/}
       </div>
   );
 }
